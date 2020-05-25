4ecc8a8e38adcd4e428c8d4cf436aef380329e79
wakson <wakson@users.noreply.github.com>
2020-05-24 23:42:26 -0700

    Delete ExternalIP.php

--- ExternalIP.php
+++
@@ -1,9 +0,0 @@
-<html>
- <head>
-  <title>Get External IP Address</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?>
- <?PHP $ip = $HTTP_SERVER_VARS["REMOTE_ADDR"]; ?>
- </body>
-</html>
